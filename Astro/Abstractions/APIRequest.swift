696facefc3f3ef3f0d69b52488ea1f1e63bae987
Michael Tabb <miketabb33@gmail.com>
2019-06-22 11:36:06 -0400

    Refactor API request abstraction

--- Astro/Abstractions/APIRequest.swift
+++ Astro/Abstractions/APIRequest.swift
@@ -6,12 +6,16 @@ class APIRequest {
     func getData(api_url: String, api_key: String, action: @escaping (JSON, @escaping () -> Void) -> (), completion: @escaping () -> Void) {
         Alamofire.request(api_url, method: .get, parameters: ["api_key" : api_key]).responseJSON {
             response in
-            if response.result.isSuccess {
-                let nasaJSON = JSON(response.result.value!)
-                action(nasaJSON, completion)
-            } else {
-                print("Error \(String(describing: response.result.error))")
-            }
+            self.dataHandler(response, action: action, completion: completion)
+        }
+    }
+    
+    func dataHandler(_ response: DataResponse<Any>, action: @escaping (JSON, @escaping () -> Void) -> (), completion: @escaping () -> Void) {
+        if response.result.isSuccess {
+            let nasaJSON = JSON(response.result.value!)
+            action(nasaJSON, completion)
+        } else {
+            print("Error \(String(describing: response.result.error))")
         }
     }
 }
