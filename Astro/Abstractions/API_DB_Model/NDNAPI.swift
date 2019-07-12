daffda868e4befac72f1a5be649b45c72abc1048
Michael Tabb <miketabb33@gmail.com>
2019-07-12 14:12:49 -0400

    Move ui abstractions into folder

--- Astro/Abstractions/API_DB_Model/NDNAPI.swift
+++ Astro/Abstractions/API_DB_Model/NDNAPI.swift
@@ -5,6 +5,7 @@ import CoreData
 
 class NDNAPI {
     lazy var persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+    let persistantData = PersistentData()
     
     func getTodaysNasaEntry() {
         Alamofire.request("https://ndn-api.herokuapp.com/", method: .get).responseJSON {
@@ -18,21 +19,12 @@ class NDNAPI {
                     todaysEntry.explanation = nasaJSON["items"][0]["explanation"].stringValue
                     todaysEntry.title = nasaJSON["items"][0]["title"].stringValue
                     todaysEntry.url = nasaJSON["items"][0]["url"].stringValue
-                    
-                    self.attemptToSaveContext(backgroundContext)
+                    self.persistantData.attemptToSaveContext(backgroundContext)
                 }
-                
             } else {
                 print("Error \(String(describing: response.result.error))")
             }
         }
     }
-    
-    func attemptToSaveContext(_ backgroundContext: NSManagedObjectContext) {
-        do {
-            try backgroundContext.save()
-        } catch {
-            print("error preloading data: \(error)")
-        }
-    }
+
 }
--- Astro/Abstractions/API_DB_Model/PersistentData.swift
+++ Astro/Abstractions/API_DB_Model/PersistentData.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 class PersistentData {
     lazy var persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
@@ -12,6 +13,14 @@ class PersistentData {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    func attemptToSaveContext(_ context: NSManagedObjectContext) {
+        do {
+            try context.save()
+        } catch {
+            print("error preloading data: \(error)")
         }
     }
     
