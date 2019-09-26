ca70c3f558b7525247fafe3263d4d3e1952673cc
Michael Tabb <miketabb33@gmail.com>
2019-09-26 12:04:08 -0400

    Remove cruft from persistent data file

--- Astro/Abstractions/DataManagement/PersistentData.swift
+++ Astro/Abstractions/DataManagement/PersistentData.swift
@@ -49,99 +49,17 @@ class PersistentData {
         return UserDefaults.standard.bool(forKey: key)
     }
     
-    //MARK: - Dead
-    let userDefaults = UserDefaults.standard
-    
-    func saveContext(_ context: NSManagedObjectContext) {
-        do {
-            try context.save()
-        } catch {
-            print("error preloading data: \(error)")
-        }
-    }
-    
-    func savePlanetData(_ backgroundContext: NSManagedObjectContext) {
-        do {
-            try backgroundContext.save()
-            userDefaults.set(true, forKey: astronomicalObjectPreloadCompletedKey)
-        } catch {
-            print("error preloading data: \(error)")
-        }
-    }
-    
-    func getPlanetData(planetContainer: [Planets]) -> [Planets] {
-        var container = planetContainer
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let request : NSFetchRequest<Planets> = Planets.fetchRequest()
-        
-        let sortDescriptor = NSSortDescriptor(key: "position", ascending: true)
-        request.sortDescriptors = [sortDescriptor]
-        
-        do {
-            container = try context.fetch(request)
-        } catch {
-            print("Error fetching data from context \(error)")
-        }
-        return container
-    }
-    
-    //MARK: - NASA Entry Feed Data Methods
-    lazy var nasaEntriesContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    func getAllNasaEntries() -> [NasaEntry] {
-        var container = [NasaEntry]()
-        let request : NSFetchRequest<NasaEntry> = NasaEntry.fetchRequest()
-        
-        let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
-        request.sortDescriptors = [sortDescriptor]
-        
-        do {
-            container = try nasaEntriesContext.fetch(request)
-        } catch {
-            print("Error fetching data from context \(error)")
-        }
-        return container
-    }
-    
-    func saveNasaEntries() {
-        if nasaEntriesContext.hasChanges {
-            do {
-                try nasaEntriesContext.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-    func insertDataIntoDatabase(stagedNasaData: [NDNAPIStagingModel]) {
-        let persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
-        let backgroundContext = persistentContainer.newBackgroundContext()
-        backgroundContext.perform {
-            for currentEntry in stagedNasaData {
-                let container = NasaEntry(context: backgroundContext)
-                container.title = currentEntry.title
-                container.date = currentEntry.date
-                container.explanation = currentEntry.explanation
-                container.url = currentEntry.url
-                self.saveContext(backgroundContext)
-            }
-            
-        }
-    }
-    
-
-    
     func cleanNasaEntryDatabase() {
-        let allNasaEntries = getAllNasaEntries()
-        for (index, entry) in allNasaEntries.enumerated() {
-            if index > 19 {
-                entry.image = nil
+        let allAPODEntries = getAllAPODEntries()
+        for (index, entry) in allAPODEntries.enumerated() {
+            try! realm.write {
+                if index > 19 {
+                    entry.image = nil
+                }
+                entry.cellHeight = 0
+                entry.expandEnabled = false
             }
-            entry.cellHeight = 0
-            entry.expandEnabled = false
         }
-        saveNasaEntries()
     }
     
 }
