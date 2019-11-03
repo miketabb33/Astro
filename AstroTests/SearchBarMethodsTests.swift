da8819ef6c57bb8326b34a6eedbb2833ad2c7f94
Michael Tabb <miketabb33@gmail.com>
2019-11-03 09:46:22 -0500

    Add test for failing search bar double validation

--- AstroTests/SearchBarMethodsTests.swift
+++ AstroTests/SearchBarMethodsTests.swift
@@ -12,13 +12,16 @@ class SearchBarMethodsTests: XCTestCase {
         searchBar = UISearchBar()
     }
     
-    //formatWeightlbs
     func test_GetValidationMessageForDouble_pass() {
         searchBar.text = "180"
         let result = SearchBarMethods(searchBar).getValidationMessageForDouble(onSuccess: pass, onFailure: fail)
         XCTAssert(result == pass, "Method should return the on seccuss text when a double is in the searchbar")
     }
     
+    func test_GetValidationMessageForDouble_fail() {
+        searchBar.text = "Not A Double"
+        let result = SearchBarMethods(searchBar).getValidationMessageForDouble(onSuccess: pass, onFailure: fail)
+        XCTAssert(result == fail, "Method should return the on failure text when not a double is in the searchbar")
+    }
 
-    
 }
