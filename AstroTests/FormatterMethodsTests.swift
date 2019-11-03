3b788a4291055048bf6d543fa797546bc9b31106
Michael Tabb <miketabb33@gmail.com>
2019-11-03 09:43:52 -0500

    Remove outdated tests

--- AstroTests/FormatterMethodsTests.swift
+++ AstroTests/FormatterMethodsTests.swift
@@ -3,27 +3,6 @@ import XCTest
 
 class FormatterMethodsTests: XCTestCase {
     
-    let formatterMethods = FormatterMethods()
-    
-    //formatWeightlbs
-    func testFormatWeightAslbsManyPlaceValues() {
-        let weight : Decimal = 50.03123
-        let value = formatterMethods.formatWeight(weight)
-        XCTAssertTrue(value == "50.03 lbs", "Should return 2 place values when many exist")
-    }
-    
-    func testFormatWeightAslbsNoPlaceValues() {
-        let weight : Decimal = 50
-        let value = formatterMethods.formatWeight(weight)
-        print(value)
-        XCTAssertTrue(value == "50 lbs", "Should return 0 place values when none exist")
-    }
-    
-    func testFormatWeightAslbs1PlaceValue() {
-        let weight : Decimal = 50.6
-        let value = formatterMethods.formatWeight(weight)
-        print(value)
-        XCTAssertTrue(value == "50.6 lbs", "Should return 0 place values when none exist")
-    }
+
     
 }
