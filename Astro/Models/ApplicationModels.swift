494a5f1ee904935e2a27aa3010a015be00bdddf6
Michael Tabb <miketabb33@gmail.com>
2019-12-16 17:53:10 -0500

    Add new model to app model

--- Astro/Models/ApplicationModels.swift
+++ Astro/Models/ApplicationModels.swift
@@ -11,6 +11,25 @@ struct APODEntryModel {
     var expandEnabled = false
 }
 
+struct APODEntry {
+    var id: Int
+    var contents: Contents
+    var feedData: FeedData
+}
+
+struct Contents {
+    var title: String
+    var explanation: String
+    var date: Date
+    var image_url: String
+}
+
+struct FeedData {
+    var image: Data
+    var cellHeight: Int
+    var expandEnabled = false
+}
+
 struct AstronomicalObjectModel: Decodable {
     var position: Int
     var name: String
